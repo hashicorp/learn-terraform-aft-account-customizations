45df9b82c1a66861f66858e503311ee1b3eca516
ritsok <msokolova13@gmail.com>
2021-11-23 11:53:17 -0500

    Account customization config

---
+++ sandbox/terraform/s3.tf
@@ -0,0 +1,6 @@
+data "aws_caller_identity" "current" {}
+
+resource "aws_s3_bucket" "sandbox_bucket" {
+  bucket = "aft-sandbox-${data.aws_caller_identity.current.account_id}"
+  acl    = "private"
+}
