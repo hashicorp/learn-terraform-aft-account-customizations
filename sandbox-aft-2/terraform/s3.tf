98fabf588a874ed86cc40084c8a3dcf2ef93dd27
Sven Lito <me@svenlito.com>
2023-08-12 12:31:52 +0700

    chore(misc): [DEVOPS-000] update customizations

--- sandbox-aft-2/terraform/s3.tf
+++ sandbox-aft-2/terraform/s3.tf
@@ -1,6 +1,8 @@
 data "aws_caller_identity" "current" {}
 
-resource "aws_s3_bucket" "sandbox_bucket" {
-  bucket = "aft-sandbox-${data.aws_caller_identity.current.account_id}"
-  acl    = "private"
+resource "aws_s3_account_public_access_block" "this" {
+  account_id              = data.aws_caller_identity.current.account_id
+  block_public_acls       = true
+  block_public_policy     = true
+  restrict_public_buckets = true
 }
