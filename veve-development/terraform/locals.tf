160d09ed69b8cf05663930aa16011707972c72b5
Sven Lito <me@svenlito.com>
2023-08-14 14:34:29 +0700

    chore(misc): [DEVOPS-000] update dev account

---
+++ veve-development/terraform/locals.tf
@@ -0,0 +1,10 @@
+locals {
+  tags = {
+    Department  = "technology"
+    Team        = "platform"
+    Schedule    = "na"
+    Tier        = "backend"
+    Environment = "development"
+    Stage       = "development"
+  }
+}
--- veve-development/terraform/main.tf
+++ veve-development/terraform/main.tf
@@ -10,20 +10,23 @@ module "terraform_cloud_oidc" {
   iam_role_name        = "terraform-cloud-oidc"
   max_session_duration = 3600
   url                  = "app.terraform.io"
+  tags                 = local.tags
 }
 
 module "aws_oidc_github" {
   source  = "unfunco/oidc-github/aws"
   version = "1.5.2"
 
-  enabled              = true
-  attach_admin_policy  = true
-  create_oidc_provider = true
-  iam_role_name        = "aws-oidc-github"
-  max_session_duration = 3600
-  iam_role_policy_arns = []
+  enabled                 = true
+  attach_admin_policy     = true
+  attach_read_only_policy = false
+  create_oidc_provider    = true
+  iam_role_name           = "aws-oidc-github"
+  max_session_duration    = 3600
   github_repositories = [
     "veve-official/infrastructure-aft",
     "veve-official/infrastructure-elasticache"
   ]
+
+  tags = local.tags
 }
